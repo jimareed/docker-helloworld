06f241344af80b45436ce3575e39a52004ab1786
jimareed <jim.a.reed@gmail.com>
2015-05-12 22:23:02 -0400

    added change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var DEFAULT_PORT = 8080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World\n');
+  res.send('Hello World 2\n');
 });
 app.listen(PORT)
 console.log('Running on http://localhost:' + PORT);
